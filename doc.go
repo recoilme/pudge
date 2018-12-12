490083b034714d984e1f1646389720dd73951c83
Vadim Kilibaba <vkulibaba@surfy.ru>
2018-12-12 15:44:58 +0300

    doc

--- doc.go
+++ doc.go
@@ -12,46 +12,6 @@
 //	)
 //
 //	func main() {
-//		ExampleSet()
-//		ExampleGet()
-//		ExampleDelete()
-//		ExampleDeleteFile()
-//		ExampleOpen()
-//	}
-//
-//	//ExampleSet lazy
-//	func ExampleSet() {
-//		pudge.Set("../test/test", "Hello", "World")
-//		defer pudge.CloseAll()
-//	}
-//
-//	//ExampleGet lazy
-//	func ExampleGet() {
-//		output := ""
-//		pudge.Get("../test/test", "Hello", &output)
-//		log.Println("Output:", output)
-//		// Output: World
-//		defer pudge.CloseAll()
-//	}
-//
-//	//ExampleDelete lazy
-//	func ExampleDelete() {
-//		err := pudge.Delete("../test/test", "Hello")
-//		if err == pudge.ErrKeyNotFound {
-//			log.Println(err)
-//		}
-//	}
-//
-//	//ExampleDeleteFile lazy
-//	func ExampleDeleteFile() {
-//		err := pudge.DeleteFile("../test/test")
-//		if err != nil {
-//			log.Panic(err)
-//		}
-//	}
-//
-//	//ExampleOpen complex example
-//	func ExampleOpen() {
 //		cfg := pudge.DefaultConfig()
 //		cfg.SyncInterval = 0 //disable every second fsync
 //		db, err := pudge.Open("../test/db", cfg)
@@ -71,15 +31,5 @@
 //		db.Get(8, &point)
 //		log.Println(point)
 //		// Output: {8 8}
-//		// Select 2 keys, from 7 in ascending order
-//		keys, _ := db.Keys(7, 2, 0, true)
-//		for _, key := range keys {
-//			var p Point
-//			db.Get(key, &p)
-//			log.Println(p)
-//		}
-//		// Output: {8 8}
-//		// Output: {9 9}
 //	}
-
 package pudge
